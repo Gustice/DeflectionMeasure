0f2da316d50a7d40dce8d56b12fc633a6ee97a59
Gustice <Jakob.Ger@gmail.com>
2019-11-23 12:37:20 +0100

    Test Files added

---
+++ MeasureDeflection/MarkerScannerTest/TryToSetTipTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MarkerScannerTest
+{
+    public class TryToSetTipTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
